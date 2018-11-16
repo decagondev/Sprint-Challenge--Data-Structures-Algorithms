4a10eaf1973fbe0e7e6153ecf2dfa65b2006ccaa
decagondev <decagondevelopment@gmail.com>
2018-11-16 16:28:12 +0000

    (MVP) : EX1 : breadth first for each : done pseudocode

--- data_structures/ex1/binary_search_tree.py
+++ data_structures/ex1/binary_search_tree.py
@@ -9,6 +9,33 @@ class BinarySearchTree:
 
   # implementation TODO: add pseudocode for this method
   def breadth_first_for_each(self, cb):
+    # perform the call back on the root value
+
+    # check the level of the node [ a nested function : lets get recursive ]
+
+      # try to set left to the nodes left property
+
+      # if it failes return an error or raise an exception
+      # if there was no exception on the try do a semi redundant call to make sure it is set
+
+      # try to set right to the nodes right property
+
+      # if it failes return an error or raise an exception
+      # if there was no exception on the try do a semi redundant call to make sure it is set
+
+      # --- end of the trys ---
+
+      # set a counter variable to 0
+
+      # if left holds a value the invoke the call back on the left value and increment the counter by 1
+
+      # if right holds a value the invoke the call back on the right value and increment the counter by 1
+
+      # if the counter hits 2 then do a recursive call to check the level of the node to the left and one to the right
+
+      # then deal with a fallout case where left or right are none and just return to the caller
+
+    # do the seed call to the nested function
     pass
 
   def insert(self, value):
