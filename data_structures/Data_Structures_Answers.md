7dfbe8f3ba2a6823f6a0f868e5c875b6cb980252
decagondev <decagondevelopment@gmail.com>
2018-11-16 17:57:00 +0000

    (MVP) : data structure answers : added answers for BFS time complexity and space complexity (also see notes in the binary_search_tree source for more in depth clarification)

--- data_structures/Data_Structures_Answers.md
+++ data_structures/Data_Structures_Answers.md
@@ -5,8 +5,10 @@ Add your answers to the questions below.
 2. What is the space complexity of your `depth_first_for_each` function?
 
 3. What is the runtime complexity of your `breadth_first_for_each` method?
+   the largest order is `O(3^n)` so we are working with an exponential order so we can simplify it to `O(C^n)`
 
 4. What is the space complexity of your `breadth_first_for_each` method?
+   and based upon the returns for the recursive functions they only ever return 1 thing therefore a space of `O(n)` is used
 
 5. What is the runtime complexity of your `heapsort` function?
 
--- data_structures/ex1/binary_search_tree.py
+++ data_structures/ex1/binary_search_tree.py
@@ -7,6 +7,7 @@ class BinarySearchTree:
   def depth_first_for_each(self, cb):
     pass    
 
+  # some refs for the O notation https://medium.com/karuna-sehgal/a-simplified-explanation-of-the-big-o-notation-82523585e835
   def breadth_first_for_each(self, cb):
     # perform the call back on the root value
     cb(self.value)
@@ -15,7 +16,7 @@ class BinarySearchTree:
     def check_level_of_node(node):
       # try to set left to the nodes left property
       try:
-        left_node = node.left 
+        left_node = node.left # O(1) operation
       except:
         # if it failes return an error or raise an exception
         return "ERROR: No Left Node" # TODO: maybe throw an exception of some sort
@@ -25,7 +26,7 @@ class BinarySearchTree:
       
       # try to set right to the nodes right property
       try:
-        right_node = node.right
+        right_node = node.right # O(1) operation
       except:
         # if it failes return an error or raise an exception
         return "ERROR: No Right Node" # TODO: maybe throw an exception of some sort
@@ -34,21 +35,21 @@ class BinarySearchTree:
         right_node = node.right 
 
       # set a counter variable to 0   
-      counter = 0
+      counter = 0 # O(1) operation
 
       # if left holds a value the invoke the call back on the left value and increment the counter by 1
       if left_node is not None: 
-        cb(left_node.value)
+        cb(left_node.value) # O(n)
         counter += 1 
 
       # if right holds a value the invoke the call back on the right value and increment the counter by 1
       if right_node is not None:
-        cb(right_node.value)
+        cb(right_node.value) # O(n)
         counter += 1 
 
       # if the counter hits 2 then do a recursive call to check the level of the node to the left and one to the right
       if counter == 2: 
-        check_level_of_node(left_node)
+        check_level_of_node(left_node) # at this point we have 2 calls to the recirsive function so this opperation set of 2 is O(2^n)
         check_level_of_node(right_node)
 
       # then deal with a fallout case where left or right are none and just return to the caller    
@@ -56,7 +57,9 @@ class BinarySearchTree:
         return
           
     # do the seed call to the nested function  
-    check_level_of_node(self)
+    check_level_of_node(self) # this call in itself does a recursive call to the chain but it is set at about O(3^n) due to the inner recursion of 3
+
+    # so all in all we can postulate that the largest order is O(3^n) so we are working with an exponential order so we can simplify it to O(C^n)
 
   def insert(self, value):
     new_tree = BinarySearchTree(value)
